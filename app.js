6f1b6be5ea8180a847dcc66f06fb68a1b849fddf
Matan Mizrahi <matanmizrahi@Matans-MacBook-Air.local>
2021-08-09 12:42:11 -0400

    getting to do list to show up

--- app.js
+++ app.js
@@ -1,1 +1,28 @@
-console.log('hi')
+// Declare Variables
+const submitButton = document.querySelector('#submit');
+const toDo = document.querySelector('.todo')
+const itemsCompleted = document.querySelector('.completed')
+
+const toDoArray = [];
+
+// Displays todo items
+const showList = () => {
+    let itemList = document.createElement('div')
+
+    let createBtn = document.createElement('button')
+    createBtn.innerText = 'Completed'
+    createBtn.setAttribute('id', 'lists')
+
+    toDoArray.forEach((item) => {
+        itemList.innerText = item
+        itemList.className = 'todo-item'
+        toDo.append(itemList)
+        itemList.append(createBtn)
+    })
+}
+
+submitButton.onclick = () => {
+    const inputValue = document.querySelector('#input-box').value
+    toDoArray.push(inputValue)
+    showList()
+}
