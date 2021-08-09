b769aaf827da214dd5428b91806a24456b788287
Matan Mizrahi <matanmizrahi@Matans-MacBook-Air.local>
2021-08-09 14:56:11 -0400

    todo list and completed task buttons work for each item

--- app.js
+++ app.js
@@ -3,16 +3,22 @@ const submitButton = document.querySelector('#submit');
 const toDo = document.querySelector('.todo')
 const itemsCompleted = document.querySelector('.completed')
 
+// create elements
+
+
+
+
 const toDoArray = [];
 
 // Displays todo items
 const showList = () => {
     let itemList = document.createElement('div')
-
+    // created 'completed' button
     let completebtn = document.createElement('button')
     completebtn.innerText = 'Completed'
     completebtn.setAttribute('id', 'lists')
 
+    // assigns the value and class name for each item in the todo array
     toDoArray.forEach((item) => {
         itemList.innerText = item
         itemList.className = 'todo-item'
@@ -35,9 +41,10 @@ const showList = () => {
 
     
 }
-
+// puts the input value into an array and runs the showlist function
 submitButton.onclick = () => {
     const inputValue = document.querySelector('#input-box').value
     toDoArray.push(inputValue)
+    document.querySelector('#input-box').value = ''
     showList()
 }
