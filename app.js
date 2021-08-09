5a4358c7c170cb59c6d4f570027b8f6e79469ea0
Matan Mizrahi <matanmizrahi@Matans-MacBook-Air.local>
2021-08-09 14:28:53 -0400

    can move items from todo to completed list and added a remove button

--- app.js
+++ app.js
@@ -9,16 +9,31 @@ const toDoArray = [];
 const showList = () => {
     let itemList = document.createElement('div')
 
-    let createBtn = document.createElement('button')
-    createBtn.innerText = 'Completed'
-    createBtn.setAttribute('id', 'lists')
+    let completebtn = document.createElement('button')
+    completebtn.innerText = 'Completed'
+    completebtn.setAttribute('id', 'lists')
 
     toDoArray.forEach((item) => {
         itemList.innerText = item
         itemList.className = 'todo-item'
         toDo.append(itemList)
-        itemList.append(createBtn)
+        itemList.append(completebtn)
     })
+    // moves todo items on to completed and added a remove button
+    completebtn.addEventListener('click', () =>{
+        itemList.remove();
+        completebtn.remove();
+        itemsCompleted.append(itemList)
+
+        const removeBtn = document.createElement('button')
+        removeBtn.innerText = 'Remove'
+        itemList.append(removeBtn)
+        removeBtn.onclick = () => {
+            itemList.remove()
+        }
+    })
+
+    
 }
 
 submitButton.onclick = () => {
